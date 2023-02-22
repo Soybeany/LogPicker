44e9d135bbccbb484be06becb7600a812b6a7502
Soybeany <soybeany@qq.com>
2023-02-22 09:17:25 +0800

    优化FileParam的逻辑实现

--- LogCollector/src/main/java/com/soybeany/log/collector/query/data/FileParam.java
+++ LogCollector/src/main/java/com/soybeany/log/collector/query/data/FileParam.java
@@ -76,7 +76,7 @@ public class FileParam {
     public void validateTime() {
         // 确保时间参数不为null
         if (null == fromTime) {
-            fromTime = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
+            fromTime = LocalDate.now().atStartOfDay();
         }
         if (null == toTime) {
             toTime = LocalDateTime.now();
