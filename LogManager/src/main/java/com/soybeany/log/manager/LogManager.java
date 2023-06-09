cf59a0b56854dfc3d2fe8d23d3ec8f65ed2507a0
Soybeany <soybeany@qq.com>
2023-06-09 10:54:13 +0800

    更正LogManager帮助信息

--- LogManager/src/main/java/com/soybeany/log/manager/LogManager.java
+++ LogManager/src/main/java/com/soybeany/log/manager/LogManager.java
@@ -22,8 +22,8 @@ public class LogManager {
     private static String getManagerHelpDirection() {
         return ""
                 + "管理参数"
-                + "\n  logSearchUrls: 使用指定参数同时查询的服务器，相当于3.0的serverIps，加上请求路径"
-                + "\n  uidSearchUrls: 需要日志关联查询(使用uid)的服务器，适用于关联分布式/微服务架构服务器上的日志"
+                + "\n  logSearchHosts: 使用指定参数同时查询的服务器，相当于3.0的serverIps，加上请求路径"
+                + "\n  uidSearchHosts: 需要日志关联查询(使用uid)的服务器，适用于关联分布式/微服务架构服务器上的日志"
                 + "\n  hideMsg: 是否需要隐藏查询情况信息，相当于3.0的hideUnimportantInfo"
                 ;
     }
